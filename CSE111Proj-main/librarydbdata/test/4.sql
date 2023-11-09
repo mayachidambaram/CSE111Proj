b1014e49c6b105875ec7d209936723eb7086cd92
mayachidambaram <101075342+mayachidambaram@users.noreply.github.com>
2023-11-09 12:56:02 -0800

    Update 4.sql

--- CSE111Proj-main/librarydbdata/test/4.sql
+++ CSE111Proj-main/librarydbdata/test/4.sql
@@ -2,3 +2,4 @@ SELECT S.s_subjectname as Subject, count(F.f_filekey) as booknuminsubj
 FROM Files F, Subjects S, ManySubjects M
 Where F.f_filekey = M.f_filekey and S.s_subjectkey = M.s_subjectkey
 Group By S.s_subjectname
+-- Displays subject and number of files pertaining to the subject
