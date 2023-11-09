ac27d197d5adb504597155999e02ae2781749b32
tgundavelli <102197109+tgundavelli@users.noreply.github.com>
2023-11-09 00:25:45 -0800

    Update 12.sql

--- CSE111Proj-main/librarydbdata/test/12.sql
+++ CSE111Proj-main/librarydbdata/test/12.sql
@@ -1,4 +1,4 @@
 Select f_title as title, strftime('%Y-%m',f_publicationyear) as date
 From Files
 Where strftime('%Y',f_publicationyear) >= '2017' and strftime('%Y',f_publicationyear) <= '2018'
-Group By strftime('%Y-%m',f_publicationyear) #revised for date range between 2017 and 2018 and edited the files dates to be year-month-day
+Group By strftime('%Y-%m',f_publicationyear)
