5ed424bbbbbb34d2dbbefa043399a8891839a7d8
mayachidambaram <101075342+mayachidambaram@users.noreply.github.com>
2023-11-09 13:00:36 -0800

    Update 17.sql

--- CSE111Proj-main/librarydbdata/test/17.sql
+++ CSE111Proj-main/librarydbdata/test/17.sql
@@ -6,3 +6,5 @@ JOIN BorrowedBooks ON Files.f_filekey = BorrowedBooks.b_book_id
 JOIN Student ON BorrowedBooks.b_student_id = Student.s_studentkey
 JOIN User ON Student.s_studentkey = User.u_userkey
 WHERE Subjects.s_subjectname = 'Computer Science';
+
+-- Based on chosen subject, displays the checked out books pertaining to that subject, when it was checked out, and who it was checked out by
