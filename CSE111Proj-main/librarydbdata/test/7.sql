cb7fc1f8429ae3d513cc4ada474303ad6aa5a145
tgundavelli <102197109+tgundavelli@users.noreply.github.com>
2023-11-08 21:11:43 -0800

    Create 7.sql

---
+++ CSE111Proj-main/librarydbdata/test/7.sql
@@ -0,0 +1,7 @@
+select F.f_filekey, F.f_title, F.f_author
+from Files F
+where F.f_publisherkey = (
+    select P.p_publisherkey 
+    From Publisher P
+    Where P.p_publishername = 'HarperCollins'
+)
