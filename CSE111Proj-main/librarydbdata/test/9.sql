f0e283fd90c1b4cf1e06d14f00b316494679a350
tgundavelli <102197109+tgundavelli@users.noreply.github.com>
2023-11-09 13:02:07 -0800

    Update 9.sql

--- CSE111Proj-main/librarydbdata/test/9.sql
+++ CSE111Proj-main/librarydbdata/test/9.sql
@@ -1,2 +1,3 @@
 SELECT DISTINCT SUBSTR(f_author, 1, INSTR(f_author, ' ') - 1) AS author_first_name
 FROM Files;
+--find author's first names that are unique so no repeats even if the author have different last names
