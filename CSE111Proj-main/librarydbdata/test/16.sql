c6dfc2dde9eafbea9508613b21b1701246289271
tgundavelli <102197109+tgundavelli@users.noreply.github.com>
2023-11-09 12:27:17 -0800

    Create 16.sql

---
+++ CSE111Proj-main/librarydbdata/test/16.sql
@@ -0,0 +1,14 @@
+select distinct User.u_firstname, User.u_lastname
+from AddedBooks
+join 
+    User ON
+    AddedBooks.a_librarian_id = User.u_userkey
+join 
+    Subjects ON
+    AddedBooks.a_subjectkey = Subjects.s_subjectkey
+join 
+    Publisher ON 
+    AddedBooks.a_publisherkey = Publisher.p_publisherkey
+join 
+    Files ON
+    AddedBooks.a_book_id = Files.f_filekey
