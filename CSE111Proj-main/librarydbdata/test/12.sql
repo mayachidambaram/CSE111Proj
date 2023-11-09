d3c05d4a3b27ef6c54046184a99f25a007e67a45
tgundavelli <102197109+tgundavelli@users.noreply.github.com>
2023-11-09 00:28:25 -0800

    Update 12.sql

--- CSE111Proj-main/librarydbdata/test/12.sql
+++ CSE111Proj-main/librarydbdata/test/12.sql
@@ -1,4 +1,4 @@
-Select f_title as title, strftime('%Y-%m',f_publicationyear) as date
+Select f_title as title, strftime('%Y-%m',f_publicationYear) as date
 From Files
-Where strftime('%Y',f_publicationyear) >= '2017' and strftime('%Y',f_publicationyear) <= '2018'
-Group By strftime('%Y-%m',f_publicationyear)
+Where strftime('%Y',f_publicationYear) >= '2017' and strftime('%Y',f_publicationYear) <= '2018'
+Group By strftime('%Y-%m',f_publicationYear)
