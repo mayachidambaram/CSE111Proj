437d7d3c007e630d1b14585a5030689cadeacfab
tgundavelli <102197109+tgundavelli@users.noreply.github.com>
2023-11-09 13:00:13 -0800

    Update 8.sql

--- CSE111Proj-main/librarydbdata/test/8.sql
+++ CSE111Proj-main/librarydbdata/test/8.sql
@@ -1,3 +1,4 @@
 SELECT count(*) as num, u_firstname as firstname, u_lastname as lastname, u_username as username
 FROM BorrowedBooks, Student, User
 WHERE u_firstname = 'Michael' and u_lastname = 'Brown' and u_userkey = s_studentkey and s_studentkey = b_student_id
+--find number of books of books student Michael Brown borrowed
