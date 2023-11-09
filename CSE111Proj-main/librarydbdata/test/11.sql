d9ae72f912dd666e2cfc2c195b63bc8ec88d7dd4
tgundavelli <102197109+tgundavelli@users.noreply.github.com>
2023-11-08 23:53:55 -0800

    Create 11.sql

---
+++ CSE111Proj-main/librarydbdata/test/11.sql
@@ -0,0 +1,13 @@
+select s.s_subjectname as sname
+from Subjects s 
+WHERE s.s_subjectkey = ( 
+    select many.s_subjectkey
+    from ManySubjects many
+    where many.f_filekey IN (  
+        select F.f_filekey
+        from Files F
+    )
+    Group By many.s_subjectkey
+    Order By count(many.f_filekey) desc 
+    limit 1 
+)
