ec65673f22489572a4df8bc82a0c24f053e5b76d
tgundavelli <102197109+tgundavelli@users.noreply.github.com>
2023-11-09 12:55:47 -0800

    Update 20.sql

--- CSE111Proj-main/librarydbdata/test/20.sql
+++ CSE111Proj-main/librarydbdata/test/20.sql
@@ -3,3 +3,5 @@ f_filekey = 7;
 
 update Files set f_filekey = f_filekey - 1
 where f_filekey > 7;
+
+--once the file with the filekey 7 is deleted, the files below it have their filekeys altered so they start at filekey 7 not filekey 8
