387422e2ec9866bb992f1e0210c078c65fd2c936
mayachidambaram <101075342+mayachidambaram@users.noreply.github.com>
2023-11-09 13:05:34 -0800

    Update 12.sql

--- CSE111Proj-main/librarydbdata/test/12.sql
+++ CSE111Proj-main/librarydbdata/test/12.sql
@@ -2,3 +2,4 @@ Select f_title as title, strftime('%Y-%m',f_publicationYear) as date
 From Files
 Where strftime('%Y',f_publicationYear) >= '2017' and strftime('%Y',f_publicationYear) <= '2018'
 Group By strftime('%Y-%m',f_publicationYear)
+-- Displays books that have been published in a certain time frame.
