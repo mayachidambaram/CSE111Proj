2b6eea36d458d690115d85ee3253bbf0a79cba4b
mayachidambaram <101075342+mayachidambaram@users.noreply.github.com>
2023-11-09 12:57:06 -0800

    Update 6.sql

--- CSE111Proj-main/librarydbdata/test/6.sql
+++ CSE111Proj-main/librarydbdata/test/6.sql
@@ -1,3 +1,4 @@
 SELECT u_firstname AS FirstName, u_lastname AS LastName, u_email AS Email, u_phonenum AS PhoneNumber
 FROM User
 WHERE u_firstname = 'John' AND u_lastname = 'Doe';
+-- Displays name and contact info of a specific user
