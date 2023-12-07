16fd356633607d7e23fda65ff6501ce465be472f
mayachidambaram <101075342+mayachidambaram@users.noreply.github.com>
2023-12-06 17:35:13 -0800

    Update 11.sql

--- CSE111Proj-main/librarydbdata/test/11.sql
+++ CSE111Proj-main/librarydbdata/test/11.sql
@@ -1,4 +1,4 @@
-select s.s_subjectname as sname
+/* select s.s_subjectname as sname
 from Subjects s 
 WHERE s.s_subjectkey = ( 
     select many.s_subjectkey
@@ -10,5 +10,16 @@ WHERE s.s_subjectkey = (
     Group By many.s_subjectkey
     Order By count(many.f_filekey) desc 
     limit 1 
-)
+) */
+
+SELECT s.s_subjectname AS sname
+FROM Subjects s
+JOIN (
+    SELECT s_subjectkey, COUNT(DISTINCT f_filekey) AS file_count
+    FROM ManySubjects
+    GROUP BY s_subjectkey
+    ORDER BY file_count DESC
+    LIMIT 1
+) AS maxFileCount ON s.s_subjectkey = maxFileCount.s_subjectkey;
+
 --finds most popular subject meaning the subject with the most books
